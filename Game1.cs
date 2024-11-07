99996a4d7dc6579b300652ae2a92a0070bdb8106
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-07 09:26:49 -0500

    qwasdzxcvfr

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,12 @@ namespace Topic_2_Scaling_and_text
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D circleTexture, rectTexture;
+        Rectangle circleRect;
+
+
+        SpriteFont TextFont;
+
 
         public Game1()
         {
@@ -19,14 +25,20 @@ namespace Topic_2_Scaling_and_text
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            
+
+            circleRect = new Rectangle(10, 100, 100, 50);
 
             base.Initialize();
+
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectTexture = Content.Load<Texture2D>("rectangle");
+            TextFont = Content.Load<SpriteFont>("TextFont");
             // TODO: use this.Content to load your game content here
         }
 
@@ -43,9 +55,13 @@ namespace Topic_2_Scaling_and_text
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+           
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(circleTexture,circleRect, Color.Brown);
+            _spriteBatch.Draw(rectTexture, new Rectangle(50, 150, 25, 75), Color.White);
+            _spriteBatch.DrawString(TextFont, "Tony", new Vector2(250, 250), Color.Black);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
